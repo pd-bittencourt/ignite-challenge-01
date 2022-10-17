e6cf3b83089aebd53a3485760d13d39222835764
Pedro Bittencourt <pedrodmbc@gmail.com>
2022-10-17 20:48:03 -0300

    Todo input styles

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -1,7 +1,19 @@
+import styles from "./Tasks.module.css";
+import { PlusCircle } from "phosphor-react";
+
 export function Tasks() {
   return (
-    <div>
-      <input type="text" />
+    <div className={styles.wrapper}>
+      <form>
+        <input
+          className={styles.todoInput}
+          type="text"
+          placeholder="Adicione uma nova tarefa"
+        />
+        <button className={styles.btnCreate}>
+          Criar <PlusCircle size={20} weight="bold" />
+        </button>
+      </form>
       <div>tarefas criadas</div>
       <div>
         <ul>
