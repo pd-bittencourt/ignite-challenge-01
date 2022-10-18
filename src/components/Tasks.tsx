7688ec2a496b068c4b2d470380296b6517930e28
Pedro Bittencourt <pedrodmbc@gmail.com>
2022-10-17 21:03:25 -0300

    Tasks counter

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -14,7 +14,17 @@ export function Tasks() {
           Criar <PlusCircle size={20} weight="bold" />
         </button>
       </form>
-      <div>tarefas criadas</div>
+
+      <div className={styles.counter}>
+        <div className={styles.tasksTotal}>
+          <span className={styles.title}>Tarefas criadas</span>
+          <div className={styles.badge}>5</div>
+        </div>
+        <div className={styles.tasksDone}>
+          <span className={styles.title}>Concluídas</span>
+          <div className={styles.badge}>2 de 5</div>
+        </div>
+      </div>
       <div>
         <ul>
           <li>tarefa 1</li>
